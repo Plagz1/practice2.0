02e71ec2201bc50155b26abd896ee5d23cae7866
Максим <zugafi@gmail.com>
2025-10-01 12:42:09 +0500

    quest4 complete

---
+++ generator.py
@@ -0,0 +1,55 @@
+import random
+import string
+
+def generate_password(length=8, use_lower=True, use_upper=True, use_digits=True, use_special=True):
+
+    characters = ""
+
+    if use_lower:
+        characters += string.ascii_lowercase  # abcdefghijklmnopqrstuvwxyz
+    if use_upper:
+        characters += string.ascii_uppercase  # ABCDEFGHIJKLMNOPQRSTUVWXYZ
+    if use_digits:
+        characters += string.digits  # 0123456789
+    if use_special:
+        characters += "!@#$%&*"
+    if characters == "":
+        characters = string.ascii_letters + string.digits + "!@#$%&*"
+
+    password = ""
+    for i in range(length):
+        password += random.choice(characters)
+
+    return password
+
+def main():
+
+    print("=== ГЕНЕРАТОР ПАРОЛЕЙ ===")
+    print()
+
+    try:
+        length = int(input("Длина пароля (рекомендуется 8-12): ") or "8")
+    except:
+        length = 8
+
+    print("\nКакие символы использовать?")
+    use_lower = input("Строчные буквы (a-z)? (да/нет) [да]: ").lower() != "нет"
+    use_upper = input("Заглавные буквы (A-Z)? (да/нет) [да]: ").lower() != "нет"
+    use_digits = input("Цифры (0-9)? (да/нет) [да]: ").lower() != "нет"
+    use_special = input("Спецсимволы (!@#$)? (да/нет) [да]: ").lower() != "нет"
+
+    password = generate_password(
+        length=length,
+        use_lower=use_lower,
+        use_upper=use_upper,
+        use_digits=use_digits,
+        use_special=use_special
+    )
+
+    print("\n" + "=" * 30)
+    print("Ваш пароль:", password)
+    print("=" * 30)
+
+
+if __name__ == "__main__":
+    main()
